39b1496b3ad85a35a94644140a0941033233cd7f
Yehor Sidelnikov <yehor_sidelnikov@epam.com>
2022-01-12 10:29:23 +0200

    WPS-0001 First Commit Of All Project

---
+++ terraform/firstone/main.tf
@@ -0,0 +1,61 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.7"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+#creating security group with ssh and http
+
+resource "aws_security_group" "mainsecgroup" {
+         name = "mainsecgroup"
+
+         ingress {
+                 from_port = 22
+                 to_port = 22
+                 protocol = "tcp"
+                 cidr_blocks = ["0.0.0.0/0"]
+         }
+         
+         ingress {
+                 from_port = 80
+                 to_port = 80
+                 protocol = "tcp"
+                 cidr_blocks = ["0.0.0.0/0"]
+
+         }
+
+         egress {
+                 from_port = 0
+                 to_port = 0
+                 protocol = "-1"
+                 cidr_blocks = ["0.0.0.0/0"]
+         }
+
+}
+
+#Create key-pair for logging into EC2 in us-east-1
+resource "aws_key_pair" "worker-key" {
+  key_name   = "wpserver"
+  public_key = file("~/.ssh/wpserver.pub")
+
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0e472ba40eb589f49"
+  instance_type = "t2.micro"
+  security_groups = ["${aws_security_group.mainsecgroup.name}"]
+  key_name = "wpserver"
+  tags = {
+    Name = "wpserver"
+  }
+} 
+
+1
