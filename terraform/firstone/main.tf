8abaeb4a48854b81d71e84c48e2bca8558d99e6e
Yehor Sidelnikov <yehor_sidelnikov@epam.com>
2022-01-13 09:50:21 +0200

    WPS-015 Revert previous commit changes and addind python destination to ansible.cfg

--- terraform/firstone/main.tf
+++ terraform/firstone/main.tf
@@ -1,14 +1,8 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.7"
-    }
-  }
+  required_version = ">=1.0"
 }
 
 provider "aws" {
-  profile = "default"
   region  = "us-east-1"
 }
 
