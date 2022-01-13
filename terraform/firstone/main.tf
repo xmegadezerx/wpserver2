36f3bd6b7f2260cf4b04f7d7662cbdaa84936a21
Yehor Sidelnikov <yehor_sidelnikov@epam.com>
2022-01-13 09:19:56 +0200

    WPS-013 Add some early changes back to test what's wrong

--- terraform/firstone/main.tf
+++ terraform/firstone/main.tf
@@ -1,8 +1,14 @@
 terraform {
-  required_version = ">=1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.7"
+    }
+  }
 }
 
 provider "aws" {
+  profile = "default"
   region  = "us-east-1"
 }
 
